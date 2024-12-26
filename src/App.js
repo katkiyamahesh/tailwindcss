1cc9b36410d98496da81c22d53da1145e7c827e1
Katkiya Mahesh <katkiyamahesh2001@gmail.com>
2024-12-26 09:34:59 +0530

    init push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-500">
+    <h1 className="text-4xl text-white font-bold">Hello, Tailwind CSS!</h1>
+  </div>
+  );
+}
+
+export default App;
